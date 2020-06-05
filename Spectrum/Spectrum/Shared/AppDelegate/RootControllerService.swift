cd08676ce119e95e593c4b2a301f84fb9e13c303
Derick <21DerickDerick@gmail.com>
2020-06-05 21:47:47 +0800

    Add IQKeyboardManager Pod

--- Spectrum/Spectrum/Shared/AppDelegate/RootControllerService.swift
+++ Spectrum/Spectrum/Shared/AppDelegate/RootControllerService.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 class RootControllerService: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
         ApplicationCoordinator.shared.start()
         ReachabilityManager.shared.start()
+        
+        IQKeyboardManager.shared.enable = true
         
         return true
     }
