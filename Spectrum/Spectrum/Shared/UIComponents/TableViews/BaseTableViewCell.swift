c7ca2150aa422ab4cb41f96caa0ba9c2bbe497d5
Derick <21DerickDerick@gmail.com>
2020-06-05 15:26:06 +0800

    Add base tableviewcontroller and base tableviewcell

---
+++ Spectrum/Spectrum/Shared/UIComponents/TableViews/BaseTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BaseTableViewCell.swift
+//  Spectrum
+//
+//  Created by Derick on 5/6/20.
+//  Copyright © 2020 DerickDev. All rights reserved.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupUI()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    private func setupUI() {
+        selectionStyle = .none
+    }
+}
---
+++ Spectrum/Spectrum/Shared/UIComponents/TableViews/BaseTableViewController.swift
@@ -0,0 +1,74 @@
+//
+//  BaseTableViewController.swift
+//  Spectrum
+//
+//  Created by Derick on 5/6/20.
+//  Copyright © 2020 DerickDev. All rights reserved.
+//
+
+import UIKit
+
+class BaseTableViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    let spinnerVC = SpinnerVC()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+        registerNIB()
+        setupUI()
+    }
+    
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.sectionHeaderHeight = UITableView.automaticDimension
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+        tableView.tableFooterView = UIView()
+    }
+    
+    func registerNIB() {
+        
+    }
+    
+    func setupUI() {
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.spectrumMain,
+            NSAttributedString.Key.font: UIFont.main,
+        ]
+        
+        navigationController?.navigationBar.setBackgroundImage(UIColor.spectrumBackground.as1ptImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIColor.spectrumFollowButton.as1ptImage()
+    }
+    
+    func showLoadingIndicator() {
+        guard let currentViewFrame = ApplicationCoordinator.shared.window.rootViewController?.view.frame else { return }
+        
+        addChild(spinnerVC)
+        spinnerVC.view.frame = currentViewFrame
+        tableView.addSubview(spinnerVC.view)
+        spinnerVC.didMove(toParent: self)
+    }
+    
+    func hideLoadingIndicator() {
+        spinnerVC.willMove(toParent: nil)
+        spinnerVC.view.removeFromSuperview()
+        spinnerVC.removeFromParent()
+    }
+}
+
+extension BaseTableViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+}
