4531f602aeae6faa5116469ebe7ac94025113510
Derick <21DerickDerick@gmail.com>
2020-06-05 15:16:32 +0800

    Add UIFonts+Additions

---
+++ Spectrum/Spectrum/Shared/UIComponents/Fonts/UIFonts+Additions.swift
@@ -0,0 +1,23 @@
+//
+//  UIFonts+Additions.swift
+//  Spectrum
+//
+//  Created by Derick on 5/6/20.
+//  Copyright © 2020 DerickDev. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    class var main: UIFont {
+        return UIFont(name: "Avenir Next Demi Bold", size: 18)!
+    }
+    
+    class var secondary: UIFont {
+        return UIFont(name: "Avenir Next Medium", size: 12)!
+    }
+    
+    class var tertiary: UIFont {
+        return UIFont(name: "Avenir Next Regular", size: 12)!
+    }
+}
