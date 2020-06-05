a39a92a4af8c0d0d5d6307c4a6057763391856de
Derick <21DerickDerick@gmail.com>
2020-06-05 14:18:09 +0800

    Add NetworkProvider

---
+++ Spectrum/Spectrum/Shared/Network/NetworkProvider.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkProvider.swift
+//  Spectrum
+//
+//  Created by Derick on 5/6/20.
+//  Copyright © 2020 DerickDev. All rights reserved.
+//
+
+import Alamofire
+
+class NetworkProvider {
+    static var shared = NetworkProvider()
+    
+    func request(url: String, type: HTTPMethod, headers: [String: String], body: [String: Any]?, completion: @escaping (Data?, AFError?) -> Void) {
+        
+        // Todo: Check for no internet
+        
+        guard let url = URL(string: url) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = type.rawValue
+        request.timeoutInterval = 60
+        request.cachePolicy = .returnCacheDataDontLoad
+        
+        let httpHeaders = HTTPHeaders(headers)
+        request.headers = httpHeaders
+        
+        if let body = body {
+            let jsonData = try? JSONSerialization.data(withJSONObject: body)
+            request.httpBody = jsonData
+        }
+        
+        AF.request(request)
+            .validate()
+            .responseJSON(completionHandler: { (response) in
+
+                switch response.result {
+                case .failure(let error):
+                    completion(nil, error)
+                case .success:
+                    completion(response.data, nil)
+                }
+        })
+    }
+}
